3e1a33d480ec239596f39185172779f5d6cce083
Dizzrt <1075570865@qq.com>
2022-03-29 21:10:07 +0800

    /

--- src/include/MMU/mempool.h
+++ src/include/MMU/mempool.h
@@ -1,28 +1,39 @@
 #ifndef __BIG_MEMPOOL_H__
 #define __BIG_MEMPOOL_H__
 
-#include "ktl\klist.h"
-#include "ktl\kbitset.h"
+// #include "ktl\klist.h"
+#include "stdint.h"
 #include "ktl\rb_tree.h"
 
-class MemPool : public kbitset {
+struct mSeg
+{
+    uint8_t flags;
+    uint8_t reserved[7]; //alignment
+
+    uint64_t base;
+    uint64_t len;
+
+    mSeg(uint64_t, uint64_t, uint8_t);
+};
+
+class MemPool {
 private:
-    uint64_t nextBase;
-    uint64_t baseLimit;
+    //memory segment node
+    typedef _rb_tree_node<uint64_t, mSeg*> mSegNode;
+
+    _rb_tree<uint64_t, mSeg*> freeSegs; // free memory segment
+    _rb_tree<uint64_t, mSeg*> usedSegs;
+
 public:
     ~MemPool() = default;
 
-    void* __alloc(uint64_t);
-};
-
-
-struct MemPoolChain
-{
-    klist<MemPool*> pools;
-
-
-    void extend(uint64_t);
+    //alloc pages
     void* _alloc(uint64_t);
+    void _free(const void*);
+
+    //base,len,flags
+    void _createSeg(uint64_t, uint64_t, uint8_t);
+    void _freeSeg(const void*);
 };
 
 
--- src/src/MMU/mempool.cpp
+++ src/src/MMU/mempool.cpp
@@ -1,1 +1,48 @@
 #include "mmu\mempool.h"
+#include "new.h"
+
+mSeg::mSeg(uint64_t _base, uint64_t _len, uint8_t _flags)
+    : base(_base), len(_len), flags(_flags) {}
+
+void MemPool::_createSeg(uint64_t _base, uint64_t _len, uint8_t _flags) {
+    mSeg* _mSeg;//TODO alloc seg
+    new (_mSeg) mSeg(_base, _len, _flags);
+
+    mSegNode* _mSegNode;//TODO alloc
+    _mSegNode->val.second = _mSeg;
+
+    freeSegs.insert(_mSegNode);
+}
+
+//TODO free seg
+
+void* MemPool::_alloc(uint64_t pages) {
+    mSeg* seg = nullptr;
+    mSegNode* segNode = nullptr;
+    uint64_t delta = UINT64_MAX;
+
+    for (auto i : freeSegs) {
+        if (i.second->len == pages) {
+            delta = 0;
+            seg = i.second;
+            // segNode = 
+            break;
+        }
+        else if (i.second->len < pages)
+            continue;
+
+        uint64_t temp = i.second->len - pages;
+        if (temp < delta) {
+            delta = temp;
+            seg = i.second;
+        }
+    }
+
+
+
+    if (delta > 0) {
+        seg->len = pages;
+
+    }
+
+}
