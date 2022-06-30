3c5a85893f5ac9df1108d128681bb2a50fae74e1
Dizzrt <1075570865@qq.com>
2022-06-30 18:40:36 +0800

    \

--- src/include/mmu/slab.h
+++ src/include/mmu/slab.h
@@ -15,124 +15,31 @@
 #define CACHE_NOEMPTY 0x00000001
 //cache flags end
 
-
-
-
 #define LONG_ALIGN(SIZE) \
   ((SIZE + sizeof(long) - 1) & ~(sizeof(long) - 1))
 
-
-#pragma region backup
-
-// class Slab : public kbitset
-// {
-// private:
-//     uint64_t base;
-//     uint64_t offsetStep;
-// public:
-//     uint32_t flags;
-//     const uint64_t& freeObjs = _fc;
-
-//     void* alloc_slab();
-
-//     //offset
-//     void free_slab(uint64_t);
-
-//     void free_slab(const void*);
-
-//     Slab(/* args */) = default;
-//     Slab(uint32_t, uint32_t, uint64_t, uint64_t, uint8_t*);
-
-
-//     ~Slab() = default;
-// };
-
-
-// struct SlabHeader
-// {
-//     Slab* slab;
-//     const uint64_t magic;
-
-//     SlabHeader(Slab*);
-// };
-// #define SHSIZE sizeof(SlabHeader)
-// #define SH_magic 0x50b7ff2785ff7b22
-
-// enum SlabSize {
-//     x1 = 1 * 0x1000, x2 = 2 * 0x1000, x4 = 4 * 0x1000,
-//     x8 = 8 * 0x1000, x16 = 16 * 0x1000, x32 = 32 * 0x1000,
-//     x64 = 64 * 0x1000, x128 = 128 * 0x1000, x256 = 256 * 0x1000,
-//     x512 = 512 * 0x1000, x1024 = 1024 * 0x1000
-// };
-
-
-// class Cache
-// {
-// private:
-//     klist<Slab*> full;
-//     klist<Slab*> empty;
-//     klist<Slab*> partial;
-
-//     uint32_t _alignment_;
-// public:
-//     uint32_t flags;
-//     uint32_t objSize;
-//     // uint32_t slabSize;
-//     SlabSize slabSize;
-
-
-//     uint32_t ops; //objs per slab
-//     uint32_t offsetStep;
-
-
-//     Cache(/* args */) = default;
-//     ~Cache() = default;
-
-//     Cache(uint32_t, uint32_t, SlabSize, uint32_t, ...);
-
-//     void* alloc_Cache();
-// };
-
-
-// class CacheChain
-// {
-// private:
-//     klist<Cache*> cache_list;
-// public:
-//     CacheChain(/* args */) = default;
-//     ~CacheChain() = default;
-
-//     void* alloc(uint64_t);
-
-//     void insert(Cache*);
-//     void insert(linked_container<Cache*>*);
-
-//     //void CreateCache();
-// };
-
 #pragma endregion
 
-
+class Cache; // forward declaration
 class Slab :public kbitset
 {
-private:
-    uint64_t base;
-
-    //size == objSize+SHSIZE (size of SlabHeader)
-    uint32_t size;
-
-    //uint64_t colour_off; 
 public:
+    uint64_t base;
+    uint64_t stepSize;
+
+    Cache* cache;
+    //uint64_t colour_off; 
+
     uint32_t flags;
     //kbitset->_len (max number of objs)
-    const uint64_t& freeObjs = _fc;
+    const uint32_t& freeObjs = _fc;
 
     void* alloc_slab(); //return a obj's address
-    void free_slab(const void*);
+    void free_slab(const void* p);
 
     Slab() = default;
     // bp_pointer == bitmap pointer
-    Slab(uint32_t Flags, uint32_t objSize, uint32_t objCnt, uint64_t base, uint8_t* bp_pointer);
+    Slab(uint32_t SlabOrder, uint32_t ObjCnt, uint64_t StepSize, Cache* _Cache = nullptr, uint32_t Flags = 0, uint64_t Base = 0, uint8_t* Bp_pointer = nullptr);
 
     ~Slab() = default;
 };
@@ -142,10 +49,10 @@ struct SlabHeader
     Slab* slab;
     const uint64_t magic;
 
-    SlabHeader(Slab*);
+    SlabHeader(Slab* _Slab);
 };
 #define SHSIZE sizeof(SlabHeader)
-#define SH_magic 0x50b7ff2785ff7b22
+#define SH_MAGIC 0x50b7ff2785ff7b22
 
 class Cache {
 public:
@@ -155,15 +62,22 @@ public:
 
     uint32_t flags;
     uint32_t objSize;  // the obj's size in byte
-    uint32_t slabSize; //index of buddyChunkPages, indicates how many pages per slab
+    uint32_t slabOrder; //index of buddyChunkPages, indicates how many pages per slab
+
+    // size == LONG_ALIGN(objSize+SHSIZE) 
+    uint32_t size;
 
     uint32_t ops; //objs per slab
+
+    void* alloc_cache();
 
     Cache() = default;
     ~Cache() = default;
 
     //SSC => static slab count
-    Cache(uint32_t Flags, uint32_t ObjSize, uint32_t SSC, ...);
+    Cache(uint32_t ObjSize, uint32_t Flags, uint32_t SSC, ...);
+
+    bool operator <(const Cache& cache) { return this->objSize < cache.objSize; }
 };
 
 class ChacheChain {
@@ -176,9 +90,9 @@ public:
     //Size -- how much space need in byte
     void* alloc(uint32_t Size);
 
-    void __insert_cache__(Cache*);
-    void __insert_cache__(linked_container<Cache*>*);
-    void __create_cache__(uint32_t Flags, uint32_t ObjSize);
+    void __insert_cache__(Cache* _cache);
+    void __insert_cache__(linked_container<Cache*>* _lcache);
+    Cache* __create_cache__(uint32_t Flags, uint32_t ObjSize);
 };
 
 
--- src/src/mmu/slab.cpp
+++ src/src/mmu/slab.cpp
@@ -1,106 +1,171 @@
 #include "mmu\slab.h"
+#include "stdarg.h"
 #include "new.h"
 
-#pragma region Slab
+void* ChacheChain::alloc(uint32_t Size) {
+    Cache* c = nullptr;
 
-SlabHeader::SlabHeader(Slab* _slab) :slab(_slab), magic(SH_magic) {}
-
-Slab::Slab(uint32_t Flags, uint32_t ObjCnt, uint64_t OffsetStep, uint64_t Base, uint8_t* Bit_ptr)
-    :kbitset(Bit_ptr, ObjCnt), flags(Flags), offsetStep(OffsetStep), base(Base)
-{
-
-}
-
-void* Slab::alloc_slab() {
-    uint64_t offset = scan(1);
-    set(offset);
-
-    offset = offset * offsetStep + base;
-    new ((SlabHeader*)offset) SlabHeader(this);
-
-    return (void*)(offset + SHSIZE);
-}
-
-void Slab::free_slab(uint64_t p) {
-    uint64_t offset = (p - base) / offsetStep;
-    reset(offset);
-}
-
-void Slab::free_slab(const void* p) { free_slab((uint64_t)p - SHSIZE); }
-
-#pragma endregion
-
-
-#pragma region Cache
-
-#include "stdarg.h"
-//Sscnt => static slab cnt
-Cache::Cache(uint32_t Flags, uint32_t ObjSize, SlabSize SSize, uint32_t Sscnt, ...) :
-    flags(Flags), objSize(ObjSize), slabSize(SSize),
-    ops((uint32_t)SSize / LONG_ALIGN(ObjSize)), offsetStep(LONG_ALIGN(ObjSize))
-{
-    va_list vlist;
-    va_start(vlist, Sscnt);
-
-    while (Sscnt--)
-    {
-        linked_container<Slab*>* _slc = va_arg(vlist, linked_container<Slab*>*);
-        empty.__list_insert(_slc);
+    for (auto i : cache_list) {
+        if (i->objSize == Size)
+            return i->alloc_cache();
+        else if (i->objSize > Size) {
+            if (c != nullptr)
+                return c->alloc_cache();
+            else return i->alloc_cache();
+        }
+        else c = i;
     }
 
-    va_end(vlist);
+    return nullptr;
 }
 
-extern Cache Cache_slab;
-void* Cache::alloc_Cache() {
-    if (partial.empty()) {
+Cache* ChacheChain::__create_cache__(uint32_t Flags, uint32_t ObjSize) {
+    //TODO if(ObjSize>slab_objsize_limit)
+
+    Cache* c = nullptr;//TODO alloc
+    new (c) Cache(ObjSize, Flags, 0);
+
+    __insert_cache__(c);
+
+    return c;
+}
+
+void ChacheChain::__insert_cache__(linked_container<Cache*>* _lcache) {
+    Cache* t = _lcache->val;
+
+    auto iter = cache_list.begin();
+    while (true)
+    {
+        if (iter == cache_list.end())
+            cache_list.__list_insert(_lcache);
+        else if ((*iter) < t) {
+            iter++;
+            continue;
+        }
+        else {
+            cache_list.__list_insert(_lcache, iter);
+            break;
+        }
+    }
+    return;
+}
+
+void ChacheChain::__insert_cache__(Cache* _cache) {
+    linked_container<Cache*>* _lc = nullptr;//TODO alloc
+    new (_lc) linked_container<Cache*>(_cache);
+
+    __insert_cache__(_lc);
+    return;
+}
+
+void* Cache::alloc_cache() {
+    if (partital.empty()) {
         if (!empty.empty()) {
             auto iter = empty.begin();
             empty.__list_remove(iter);
-            partial.__list_insert(iter);
+            partital.__list_insert(iter);
         }
         else {
             //TODO new slab
-
         }
 
-        if (flags & CACHE_NOEMPTY && empty.empty()) {
-            //TODO new slab to empty
-        }
+        //TODO check flags here
+        // if (flags & CACHE_NOEMPTY && empty.empty()) {
+        //     //TODO new slab to empty
+        // }
     }
 
-    //list partial has at least one slab
-    auto iter = partial.begin();
+    //partial list has at least one slab
+    auto iter = partital.begin();
     Slab* s = *iter;
 
     void* ret = s->alloc_slab();
     if (s->freeObjs == 0) {
-        partial.__list_remove(iter);
+        partital.__list_remove(iter);
         full.__list_insert(iter);
     }
 
     return ret;
 }
 
-#pragma endregion
+Cache::Cache(uint32_t ObjSize, uint32_t Flags, uint32_t SSC, ...)
+    : objSize(ObjSize), flags(Flags), size(LONG_ALIGN(ObjSize + SHSIZE))
+{
+    uint64_t cur;
+    uint64_t delta = INT64_MAX;
+    for (uint32_t i = 0;i < BUDDY_MAX_ORDER;i++) {
+        cur = buddyChunkSize[i] - (buddyChunkSize[i] / size) * size;
 
+        if (cur < delta) {
+            slabOrder = i;
+            delta = cur;
+        }
+    }
 
-#pragma region CacheChain
+    ops = buddyChunkSize[slabOrder] / size;
 
-void* CacheChain::alloc(uint64_t len) {
-    for (auto i : cache_list)
-        if (i->objSize >= len) {
-            return i->alloc_Cache();
+    va_list vlist;
+    va_start(vlist, SSC);
+
+    while (SSC--)
+    {
+        linked_container<Slab*>* _lc = va_arg(vlist, linked_container<Slab*>*);
+        empty.__list_insert(_lc);
+    }
+
+    va_end(vlist);
+}
+
+//TODO adjust
+Slab::Slab(uint32_t SlabOrder, uint32_t ObjCnt, uint64_t StepSize, Cache* _Cache,
+    uint32_t Flags, uint64_t Base, uint8_t* Bp_pointer)
+    : kbitset(Bp_pointer == nullptr ? nullptr/*TODO alloc Bp_pointer*/ : Bp_pointer, ObjCnt),
+    base(Base == 0 ? 0/*TODO alloc base*/ : Base), cache(_Cache), flags(Flags), stepSize(StepSize) {}
+
+void* Slab::alloc_slab() {
+    uint64_t offset = scan(1);
+    set(offset);
+
+    offset = offset * stepSize + base;
+    new ((SlabHeader*)offset) SlabHeader(this);
+
+    return (void*)(offset + SHSIZE);
+}
+
+void Slab::free_slab(const void* p) {
+    uint64_t offset = (uint64_t)p;
+    offset = (offset - SHSIZE - base) / stepSize;
+
+    if (cache != nullptr) {
+        klist<Slab*>::iterator iter;
+
+        if (!_fc)
+            iter = cache->full.begin();
+        else iter = cache->partital.begin();
+
+        while (true)
+        {
+            if ((*iter) == this)
+                break;
+
+            else iter++;
         }
 
-    //TODO param
-    return nullptr;
+        if (!_fc) {
+            cache->full.__list_remove(iter);
+
+            if (_fc + 1 == _len)
+                cache->empty.__list_insert(iter);
+            else cache->partital.__list_insert(iter);
+        }
+        else if (_fc + 1 == _len) {
+            cache->partital.__list_remove(iter);
+            cache->empty.__list_insert(iter);
+        }
+    }
+
+    reset(offset);
 }
 
-void CacheChain::insert(Cache* cache_ptr) {
-    //for(auto i:)
-}
-
-void CacheChain::insert(linked_container<Cache*>* lcache_ptr) {}
-
-#pragma endregion
+SlabHeader::SlabHeader(Slab* _Slab)
+    :slab(_Slab), magic(SH_MAGIC) {}
