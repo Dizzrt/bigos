491eaae485859f34fe1a7945ae6da0eb1e24a867
Dizzrt <1075570865@qq.com>
2022-07-24 03:14:53 +0800

    \

--- src/include/ktl/ktl_allocator.h
+++ src/include/ktl/ktl_allocator.h
@@ -1,6 +1,20 @@
 #ifndef __BIG_KTL_ALLOCATOR_H__
 #define __BIG_KTL_ALLOCATOR_H__
 
+#include "stddef.h"
+
+template<typename _Tp>
+class __kallocator_base {
+public:
+    typedef _Tp* pointer;
+
+
+    virtual pointer allocate(size_t __n) noexcept = 0;
+    virtual void deallocate(pointer __p) noexcept = 0;
+    virtual void construct() noexcept = 0;
+    virtual void destory() noexcept = 0;
+};
+
 
 
 #endif
