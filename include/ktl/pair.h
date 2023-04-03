4539d2d44b252dc1c21f575dbeef00457722af44
Dizzrt <dizzrt.s@qq.com>
2023-04-03 23:40:39 +0800

    add pair

---
+++ include/ktl/pair.h
@@ -0,0 +1,64 @@
+//
+// File: pair.h
+// Created by osspace on 2023/04/02.
+//
+// Copyright (C) 2023 The BigOS Authors.
+// Licensed under the GNU General Public License v3.0 only.
+//
+
+#ifndef _BIG_PAIR_H
+#define _BIG_PAIR_H
+
+#include <stddef.h>
+
+namespace ktl {
+    template <typename _Tp1, typename _Tp2>
+    struct pair {
+        typedef _Tp1 first_type;
+        typedef _Tp2 second_type;
+
+        _Tp1 first;
+        _Tp2 second;
+
+        pair(const _Tp1& _first, const _Tp2& _second) : first(_first), second(_second) {}
+        pair(const pair<_Tp1, _Tp2>& _pair) : pair(_pair.first, _pair.second) {}
+        pair() : pair(_Tp1(), _Tp2()) {}
+    };
+
+    template <typename _Tp1, typename _Tp2>
+    inline constexpr bool operator==(const pair<_Tp1, _Tp2>& x, const pair<_Tp1, _Tp2>& y) {
+        return x.first == y.first && x.second == y.second;
+    }
+
+    template <typename _Tp1, typename _Tp2>
+    inline constexpr bool operator!=(const pair<_Tp1, _Tp2>& x, const pair<_Tp1, _Tp2>& y) {
+        return !(x == y);
+    }
+
+    template <typename _Tp1, typename _Tp2>
+    inline constexpr bool operator<(const pair<_Tp1, _Tp2>& x, const pair<_Tp1, _Tp2>& y) {
+        return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
+    }
+
+    template <typename _Tp1, typename _Tp2>
+    inline constexpr bool operator>(const pair<_Tp1, _Tp2>& x, const pair<_Tp1, _Tp2>& y) {
+        return y < x;
+    }
+
+    template <typename _Tp1, typename _Tp2>
+    inline constexpr bool operator<=(const pair<_Tp1, _Tp2>& x, const pair<_Tp1, _Tp2>& y) {
+        return !(y < x);
+    }
+
+    template <typename _Tp1, typename _Tp2>
+    inline constexpr bool operator>=(const pair<_Tp1, _Tp2>& x, const pair<_Tp1, _Tp2>& y) {
+        return !(x < y);
+    }
+
+    template <typename _Tp1, typename _Tp2>
+    inline pair<_Tp1, _Tp2> make_pair(const _Tp1& x, const _Tp2& y) {
+        return pair<_Tp1, _Tp2>(x, y);
+    }
+}   // namespace ktl
+
+#endif   // _BIG_PAIR_H
